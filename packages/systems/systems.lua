ca81a78714518a69ed1d5e10595309207be67981
Jonathan Green <dev.jonathan.green@gmail.com>
2024-09-11 02:28:32 -0400

    added handler to fetch feed

--- packages/systems/systems.lua
+++ packages/systems/systems.lua
@@ -11,6 +11,23 @@ function filters(value)
     return filters
 end
 
+function fetch(tbl, page, size)
+    local tempArray = {}
+    for k, v in pairs(tbl) do
+        table.insert(tempArray, v)
+    end
+    local start = (page - 1) * size + 1
+    local endPage = page * size
+    local result = {};
+    for i = start, endPage do
+        if tempArray[i] then
+            table.insert(result, tempArray[i])
+        else
+            break
+        end
+    end
+    return result;
+end
 
 return {
     event = event,
